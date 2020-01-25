35f7862bc8461876b5ebfc28a210c4beb97ec24f
fn <46826941+ForgivenNin@users.noreply.github.com>
2020-01-25 12:35:50 -0500

    finish decode

--- text2hex.js
+++ text2hex.js
@@ -1,3 +1,4 @@
+// Purposefully done without the "%" (except in order to decrypt)
 function text2hex(text, mode) {
   ret = "";
   if (mode === "c") {
@@ -5,11 +6,16 @@ function text2hex(text, mode) {
       var hex = Number(text.charCodeAt(n)).toString(16).toUpperCase();
       ret += hex;
     }
+    return ret;
   } else if (mode === "r") {
-    for (var n = 0; n < text.length; n++) {
-      var hex = Number(text.charCodeAt(n)).toString(10);
-      ret += hex;
+    var text2 = text.split("")
+    text2.unshift("")
+    for (var n = 0; n < text.length; n += 2) {
+		  text2[n] += "%"
     }
+    var text3 = text2.join()
+    var text4 = text3.replace(/,/g, "")
+    var text5 = decodeURI(text4);
   }
-  return ret;
+  return text5
 }
